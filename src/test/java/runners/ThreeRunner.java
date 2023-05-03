115d95e134be47bb62ef6495c22820a5efa7d73a
Paul Phatharacharukul <paulppath@gmail.com>
2023-05-03 01:35:15 -0400

    first commit

---
+++ src/test/java/runners/ThreeRunner.java
@@ -0,0 +1,22 @@
+package runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "pretty",
+                "html:target/report3.html",
+                "json:target/cucumber-reports/cucumber3.json",
+                "rerun:target/rerun3.txt"
+        },
+        features = "src/test/resources/features",
+        glue = "step_definitions",
+        stepNotifications = true,
+        dryRun = false
+        ,tags = "@US1021"
+)
+public class ThreeRunner {
+}
